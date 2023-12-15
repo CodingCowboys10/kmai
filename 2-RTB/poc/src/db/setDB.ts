5b2252f968b7beecc0611ca539b9c25ae61cae8a
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2023-12-15 10:45:25 +0100

    Add database
    Aggiunto il database sqlite3 databaseDoc.db, il quale contiene le info di tabella.pdf, anchessa aggiunta alla cartella /docs. Aggiunto anche setDB.ts che mostra tutte le operazioni eseguibili sul database

---
+++ 2-RTB/poc/src/db/setDB.ts
@@ -0,0 +1,77 @@
+//file con cui si è settato il database. Contiene tutte le operazioni eseguibili nel db sqlite3
+
+/*
+let sql;
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database("./databaseDoc.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+});
+*/
+
+/*
+sql = 'CREATE TABLE documents(name TEXT PRIMARY KEY, path TEXT, date DATE DEFAULT CURRENT_DATE)';
+db.run(sql);
+*/
+
+/*
+// per inserire un nuovo documento in database
+sql = 'INSERT INTO documents(name,path) VALUES (?,?)';
+db.run(
+    sql,
+    ['tabella.pdf','./src/db/docs/tabella.pdf'],
+    (err) => {
+        if (err) return console.error(err.message);
+    });
+*/
+
+/*
+// restituisce tutti i documenti in database
+sql = 'SELECT * FROM documents';
+db.all(sql, [], (err, rows) =>  {
+    if (err) return console.error(err.message);
+    rows.forEach((row) => {
+        console.log(row);
+    })
+})
+*/
+
+
+/*
+let results = [];
+sql = 'SELECT * FROM documents';
+db.all(sql, [], (err, rows) =>  {
+    if (err) return console.error(err.message);
+    
+    // Aggiungi ogni riga ai risultati
+    rows.forEach((row) => {
+        results.push(row);
+    });
+
+    // Ora results contiene tutti i risultati della query
+    console.log(results);
+});
+*/
+
+
+//db.run('DROP TABLE documents');
+
+/*
+// rimuovere un documento dal database
+sql = 'DELETE FROM documents WHERE name=?';
+db.run(
+    sql,
+    ['tabella.pdf'],
+    (err) => {
+        if (err) return console.error(err.message);
+    });
+*/
+
+/*
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Closed the database connection.");
+  });
+  */
