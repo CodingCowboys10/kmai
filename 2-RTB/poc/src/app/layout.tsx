cce171455a0b2c18d4dbcfac30501a38c5d7d258
Menny <giovanni.menon.dev@gmail.com>
2023-12-18 01:03:27 +0100

    Tailwind and Template

--- 2-RTB/poc/src/app/layout.tsx
+++ 2-RTB/poc/src/app/layout.tsx
@@ -9,6 +9,12 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+        <head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+                  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+                  crossOrigin="anonymous" referrerPolicy="no-referrer"/>
+            <title>KMAI</title>
+        </head>
         <body className={inter.className}>{children}</body>
         </html>
     )
--- 2-RTB/poc/src/app/page.tsx
+++ 2-RTB/poc/src/app/page.tsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 export default function Page() {
     return (
         <main id="root">
-            <div className="grid gap-20 grid-cols-2">
+            <div className="grid gap-80 grid-cols-2 h-full">
                 <Link href="/documentazione">
-                    <div className="border-white border-2 rounded-2xl border-opacity-20 p-5 ">
-                        <h1 className="text-4xl text-white">Doc</h1>
-                    </div>
+                    <button className="w-full border-white border-2 rounded-2xl border-opacity-20 p-7 cursor-pointer">
+                        <i className="fa-solid fa-database fa-2xl"></i>
+                        <p className="text-2xl mt-5">Archivio Documenti</p>
+                    </button>
                 </Link>
 
 
                 <Link href="/chat">
-                    <div className="border-white border-2 rounded-2xl border-opacity-20 p-5 ">
-                        <h1 className="text-4xl text-white">Chat</h1>
-                    </div>
+                    <button className=" w-full border-white border-2 rounded-2xl border-opacity-20 p-7 cursor-pointer">
+                        <i className="fa-solid fa-comments fa-2xl"></i>
+                        <p className="text-2xl mt-5">Chat</p>
+                    </button>
                 </Link>
-
-
 
 
             </div>
