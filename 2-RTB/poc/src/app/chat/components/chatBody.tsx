88d9445079039aed986b9292f208ad8cae314ee0
Menny <giovanni.menon.dev@gmail.com>
2023-12-21 23:11:22 +0100

    piccolo fix

--- 2-RTB/poc/src/app/chat/components/chatBody.tsx
+++ 2-RTB/poc/src/app/chat/components/chatBody.tsx
@@ -1,6 +1,5 @@
 import React , {useRef, useEffect} from "react";
-import ChatMessageAsk from "@/app/chat/components/chatMessageAsk";
-import ChatMessageAnswer from "@/app/chat/components/chatMessageAnswer";
+import ChatMessage from "@/app/chat/components/chatMessage";
 
 interface chatMessage{
     id : number,    // 0 == ask / 1 == answer
@@ -23,14 +22,21 @@ function ChatBody({list,clearChat}:ChatProps) {
     return (
         <>
 
-            <div id="chatArea" className="font-sans overflow-y-scroll border-2 rounded-xl border-slate-900 p-2 mb-5" style={{minWidth: '55em',minHeight: '32em',maxWidth: '55em',maxHeight: '32em'}}>
-                <div className="mt-16">{list.map((value, index) => (
+            <div className={`${list.length == 0 ? "justify-around" : "" } overflow-scroll h-full rounded-2xl p-2 mb-5 text-center flex flex-col`}>
+
+                {list.length ==0 && (<div className="text-[--text] opacity-70 font-medium text-2xl mt-32 transition duration-300 ease-in">Ancora nessun Messaggio</div>)}
+
+                <div className="flex flex-col ">
+                    {list.map((value, index) => (
                     <React.Fragment key={index}>
-                        {value.id === 0 && (<ChatMessageAsk text={value.text}/>)}
-                        {value.id === 1 && (<ChatMessageAnswer text={value.text}/>)}
+                        {value.id === 0 && (<ChatMessage isGenerated={false} text={value.text}/>)}
+                        {value.id === 1 && (<ChatMessage isGenerated={true} text={value.text}/>)}
                     </React.Fragment>
                 ))}</div>
-                <button className="absolute top-4 right-8" onClick={clearChat}>Clear chat</button>
+
+                {list.length!=0 && (<button className="text-xl p-2 " onClick={clearChat}>
+                    <i className="py-2 rounded-2xl w-10/12 text-center bg-[--background-contrast] fa-solid fa-broom"></i>
+                </button>)}
                 <div ref={chatboxRef}></div>
             </div>
 
