905c8f74a2dfc0df8abadfe02cf8308e9c336b17
AnnaNordio <121154429+AnnaNordio@users.noreply.github.com>
2024-01-16 10:24:43 +0100

    fix path

--- actions/gloss.py
+++ actions/gloss.py
@@ -5,7 +5,7 @@ glossary = list()
 singolare=list()
 plurale=list()
 currentDirectory = os.environ['CURRENT_DIRECTORY']+"/"
-glossarioPath = os.environ['GLOSSARIO_PATH']
+glossarioPath = os.environ['GLOSSARIO_PATH']+"/"
 def build_file_path_letters(): #prende tutti i file delle lettere e salva in una lista i vari path
 
     fp=list()
