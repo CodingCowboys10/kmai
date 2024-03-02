e2a98d2dec0a9002ba3778204f33a9cdfdbb95a6
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-02 13:41:16 +0100

    Add documents page
    Presenta:
    - sidebar con settings dell'applicazione (ha senso sia identica a quella presente in chat)
    - form di upload dei documenti (già presente drag and drop, già presente blocco dei documenti che non sono .pdf, da aggiungere un toast che spiega perché non ha messo un file che non è pdf)
    - pulsante di upload, ovviamente senza logica implementata
    - visualizzazione documenti, mostrando per ognuno nome, data e size
    - menù di azioni per ogni documento, con tasto per apertura url e eliminazione documento (già implementata la richiesta di conferma)
    - barra di ricerca, per filtrare real time i documenti in base al nome immesso
    Per fare i test grafici, immessi dati di test
    
    Modificati alcuni componenti della chat, come una frase nel settings (dato che secondo me va messa fuori da components/chat)

---
+++ 3-PB/mvp/src/components/documents/listDocItems.tsx
@@ -0,0 +1,102 @@
+"use client"
+import React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import { Input } from "@/components/ui/input"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
+  })
+
+  return (
+    <div>
+    <div className="flex items-center py-4">
+      <Input
+        placeholder="Cerca per nome..."
+        value={(table.getColumn("id")?.getFilterValue() as string) ?? ""}
+        onChange={(event) =>
+          table.getColumn("id")?.setFilterValue(event.target.value)
+        }
+        className="max-w-sm"
+      />
+    </div>
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                Nessun documento.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+    </div>
+  )
+}
