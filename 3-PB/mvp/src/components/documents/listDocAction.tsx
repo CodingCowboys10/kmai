219cf9ac55e2e827355da7790feb5ada5b250a53
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-04 17:17:41 +0100

    Add listDocAction.tsx and deleteDoc.tsx
    Aumentata modularità componenti

---
+++ 3-PB/mvp/src/components/documents/listDocAction.tsx
@@ -0,0 +1,38 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import DeleteDoc from "./deleteDoc";
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+interface IDoc{
+    name: string;
+    url: string
+}
+
+export default async function ListDocAction({ name, url }: IDoc) {
+  
+    return (
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Azioni</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Azioni</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={() => window.open(url, "_blank")}
+          >Visualizza</DropdownMenuItem>
+          <DeleteDoc name={name}/>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
