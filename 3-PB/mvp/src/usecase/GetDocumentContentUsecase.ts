09c1784441b29bbbfd80b1aa6518f2ebab687549
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-07 12:20:59 +0100

    Add files via upload

---
+++ 3-PB/mvp/src/usecase/GetDocumentContentUsecase.ts
@@ -0,0 +1,18 @@
+import type {IDocumentRepository, IUsecase} from "@/utils/interfaces";
+import {inject, injectable} from "tsyringe";
+
+@injectable()
+class GetDocumentContentUsecase implements IUsecase<{ docName: string, model: string }, Promise<string>>{
+    private readonly _documentRepository : IDocumentRepository;
+
+    constructor( @inject("documentRepository") documentRepository: IDocumentRepository) {
+        this._documentRepository = documentRepository;
+    }
+
+    async execute({docName, model} : {docName: string, model: string}) {
+        return await this._documentRepository.getDocumentContent(docName, model);
+    }
+
+}
+
+export {GetDocumentContentUsecase}
