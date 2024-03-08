2b946c9140fe284eed9d203778a2247862d8649f
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-08 22:08:05 +0100

    Update container.ts

--- 3-PB/mvp/src/lib/config/container.ts
+++ 3-PB/mvp/src/lib/config/container.ts
@@ -11,32 +11,17 @@ import { AddDocumentUsecase } from "@/usecase/document/AddDocumentUsecase";
 import { DeleteDocumentUsecase } from "@/usecase/document/DeleteDocumentUsecase";
 import { GetDocumentContentUsecase } from "@/usecase/document/GetDocumentContentUsecase";
 import { GetDocumentsUsecase } from "@/usecase/document/GetDocumentsUsecase";
-import { ChromaDataSource } from "@/infrastructure/data-source/ChromaDataSource";
-import { ChromaClient } from "chromadb";
-import { EmbeddingRepository } from "@/infrastructure/embeddingRepository";
-import { AddEmbeddingUsecase } from "@/usecase/embeddings/AddEmbeddingUsecase";
+
 
 const AWSParams = {
-  endpoint: process.env.AWS_ENDPOINT_URL,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  endpoint: 'http://172.17.0.2:9000',
+  accessKeyId: "ROOTUSER" ,
+  secretAccessKey : "CHANGEME123",
   s3ForcePathStyle: true,
-};
+}
+
 
 container.register<AWS.S3>("s3", { useValue: new AWS.S3(AWSParams) });
-
-container.register<ChromaClient>("chromaClient", {
-  useValue: new ChromaClient(),
-});
-container.register<ChromaDataSource>("embeddingDataSource", {
-  useClass: ChromaDataSource,
-});
-container.register<EmbeddingRepository>("embeddingRepository", {
-  useClass: EmbeddingRepository,
-});
-container.register<AddEmbeddingUsecase>("addEmbeddingUsecase", {
-  useClass: AddEmbeddingUsecase,
-});
 
 container.register<MinioDataSource>("documentDataSource", {
   useClass: MinioDataSource,
@@ -53,9 +38,11 @@ container.register<AddDocumentUsecase>("addDocUsecase", {
 container.register<DeleteDocumentUsecase>("delDocUsecase", {
   useClass: DeleteDocumentUsecase,
 });
+
 container.register<GetDocumentContentUsecase>("getDocContUsecase", {
   useClass: GetDocumentContentUsecase,
 });
+
 container.register<GetDocumentsUsecase>("getDocsUsecase", {
   useClass: GetDocumentsUsecase,
 });
@@ -63,13 +50,10 @@ container.register<GetDocumentsUsecase>("getDocsUsecase", {
 const addDocumentController = container.resolve(AddDocumentController);
 const deleteDocumentController = container.resolve(DeleteDocumentController);
 const getDocumentsController = container.resolve(GetDocumentsController);
-const getDocumentContentController = container.resolve(
-  GetDocumentContentController,
-);
+const getDocumentContentController = container.resolve(GetDocumentContentController);
 
 export {
   addDocumentController,
   deleteDocumentController,
   getDocumentsController,
-  getDocumentContentController,
-};
+  getDocumentContentController};
