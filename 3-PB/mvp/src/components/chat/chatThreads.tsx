64b850a333fa5a042580b892d37075bd3e8cdb00
Francesco Giacomuzzo <giacomuzzofrancesco3@gmail.com>
2024-03-08 11:01:38 +0100

    fix button

--- 3-PB/mvp/src/components/chat/chatThreads.tsx
+++ 3-PB/mvp/src/components/chat/chatThreads.tsx
@@ -11,12 +11,16 @@ export function ChatThreads() {
   return (
     <ScrollArea className="h-full w-full rounded-md border">
       <div className="p-3">
-        <div className="mb-2 flex justify-between items-center">
-          <h4 className=" text-sm font-medium leading-none">Threads</h4>
-          <Button variant="outline" className="bg-sky-700 hover:bg-sky-400">
+        <div className="mb-2 flex flex-col items-center justify-center">
+          <Button
+            variant="outline"
+            className="bg-sky-700 hover:bg-sky-400 mb-2"
+          >
             Nuova chat
           </Button>
         </div>
+        <h4 className="mb-4 text-sm font-medium leading-none">Threads</h4>
+
         {tags.map((tag) => (
           <React.Fragment key={tag}>
             <div className="text-sm">
