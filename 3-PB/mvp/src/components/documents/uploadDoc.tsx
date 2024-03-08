6e54c307a9de2e6dadd4ac17707eb30a085aa9ba
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-08 22:09:16 +0100

    Update uploadDoc.tsx

--- 3-PB/mvp/src/components/documents/uploadDoc.tsx
+++ 3-PB/mvp/src/components/documents/uploadDoc.tsx
@@ -8,7 +8,38 @@ function UploadDoc() {
   const handleFileChange = async (event : any)  => {
     const file = event.target.files[0];
     if (file && file.type === "application/pdf") {
+        const data = new FormData();import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import {addDocumentController} from "@/lib/config/container";
+
+function UploadDoc() {
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/pdf") {
         const data = new FormData();
+        data.set('file', file);
+        data.set('model', 'openAi')
+        addDocumentController.handle(data)
+    } else {
+      event.target.value = null;
+    }
+  };
+
+  return (
+    <div className="p-1 pt-5">
+      <div className="grid w-full max-w-sm items-center justify-center gap-1.5 h-full">
+        <Label className="text-center w-full">Aggiungi PDF</Label>
+        <Input type="file" accept="application/pdf" onChange={handleFileChange} />
+      </div>
+      <Button className="w-full p-1">Invia</Button>
+    </div>
+  );
+}
+
+export default UploadDoc;
+
         data.set('file', file);
         data.set('model', 'openAi')
         const res = await addDocumentController.handle(data)
