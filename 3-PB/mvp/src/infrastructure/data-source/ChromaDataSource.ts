c276933fb2a132a1fa7a99318c65dec84bcd10e4
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-10 20:13:14 +0100

    Update ChromaDataSource.ts

--- 3-PB/mvp/src/infrastructure/data-source/ChromaDataSource.ts
+++ 3-PB/mvp/src/infrastructure/data-source/ChromaDataSource.ts
@@ -1,19 +1,18 @@
-import {
-  Embeddings,
-  IEmbeddingDataSource,
-  Metadatas,
-} from "@/lib/config/interfaces";
+import { ChromaClient } from "chromadb";
+import { collections } from "@/lib/site-config";
+import { Embeddings, IEmbeddingDataSource, Metadatas } from "@/lib/config/interfaces";
+import { injectable, inject } from "tsyringe";
 
+@injectable()
 class ChromaDataSource implements IEmbeddingDataSource {
-  constructor() {}
+  private readonly _vDb: ChromaClient;
 
-  async addOne({
-    embeddings,
-    model,
-  }: {
-    embeddings: Embeddings;
-    model: string;
-  }) {
+  constructor(@inject("chromaclient") vDb: ChromaClient) {
+    this._vDb = vDb;
+  }
+
+  async addOne({ embeddings, model }: { embeddings: Embeddings; model: string }) {
+  /*
     const res = await fetch("/api/document/embedding/addOne", {
       method: "POST",
       headers: {
@@ -24,9 +23,19 @@ class ChromaDataSource implements IEmbeddingDataSource {
         model: model,
       }),
     });
+  */
+    const collection = await this._vDb.getCollection({ name: collections[model] });
+    await collection.add({
+      ids: embeddings.ids,
+      documents: embeddings.doc,
+      metadatas: embeddings.metadata,
+      embeddings: embeddings.embedding,
+    });
+
   }
 
   async deleteOne({ ids, model }: { ids: string[]; model: string }) {
+  /*
     const res = await fetch("/api/document/embedding/deleteOne", {
       method: "POST",
       headers: {
@@ -37,27 +46,19 @@ class ChromaDataSource implements IEmbeddingDataSource {
         model: model,
       }),
     });
+  */
+    const collection = await this._vDb.getCollection({ name: collections[model] });
+    await collection.delete({ ids: ids });
   }
 
-  async updateOne({
-    metadatas,
-    model,
-    ids,
-  }: {
-    metadatas: Metadatas;
-    model: string;
-    ids: string[];
-  }) {
-    /*
-    const collection = await this._vDb.getCollection({
-      name: collections[model],
-    });
+  async updateOne({ metadatas, model, ids }: { metadatas: Metadatas; model: string; ids: string[] }) {
+    
+    const collection = await this._vDb.getCollection({ name: collections[model] });
     await collection.update({
       ids: ids,
       metadatas: metadatas,
     });
-
-     */
+     
   }
 }
 
