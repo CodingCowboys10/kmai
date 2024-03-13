88cec6484d361524dc0e2f31b7776b070c7bb704
Menny <giovanni.menon.dev@gmail.com>
2024-03-13 17:19:01 +0100

    chat

--- 3-PB/mvp/src/components/documents/docTable.tsx
+++ 3-PB/mvp/src/components/documents/docTable.tsx
@@ -1,9 +1,12 @@
 import { DocumentInfo, columns } from "./docContent";
 import { DataTable } from "./dataTable";
 import { getDocumentsController } from "@/lib/config/container";
+import { useEffect, useState } from "react";
 
 export default async function DocTable() {
-  const data = await (await getDocumentsController.handle("Ollama")).json();
+  const [data, setData] = useState([]);
+
+  setData(await (await getDocumentsController.handle("Ollama")).json());
 
   return (
     <div className="container mx-auto py-10">
