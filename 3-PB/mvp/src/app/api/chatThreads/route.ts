71680cb3f0371e962ced4d3248d96e308b682e65
checco296 <francescovillorba@gmail.com>
2024-03-14 22:32:06 +0100

    modifica

--- 3-PB/mvp/src/app/api/chatThreads/route.ts
+++ 3-PB/mvp/src/app/api/chatThreads/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import pool from "@/app/api/utils/postgres";
 
-export default async function chatThreads(req: NextRequest) {
+//funzione per ottenere i titoli delle chat
+export async function GET(req: NextRequest) {
     try {
         const query = 'SELECT title FROM chat_threads';
         const { rows } = await pool.query(query);
@@ -9,9 +10,12 @@ export default async function chatThreads(req: NextRequest) {
         const titles = rows.map((row: any) => ({
             title: row.title,
         }));
-        return NextResponse.json({ titles }, { status: 200 });
+        return NextResponse.json( titles , { status: 200 });
     } catch (error) {
         console.error(error);
-        return NextResponse.json({ success: false, error: 'Internal Server Error' }, { status: 500 });
+        return NextResponse.json(
+            { success: false, error: 'Internal Server Error' },
+            { status: 500 }
+        );
     }
 }
--- 3-PB/mvp/src/components/chat/chatThreads.tsx
+++ 3-PB/mvp/src/components/chat/chatThreads.tsx
@@ -2,23 +2,32 @@ import * as React from "react";
 
 import { Pencil2Icon, DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
+//import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
-import pool from "@/app/api/chat/chatThread";
+import {useEffect,useState} from "react";
 import { Label } from "@/components/ui/label";
 
-const tags = Array.from({ length: 50 }).map(
-  (_, i, a) => `thread numero ${a.length - i}`,
-);
 
-export function ChatThreads() {
+/*export function ChatThreads() {
+    const [title, setTitle] = useState<string>("");
+    useEffect(() => {
+            const fetchTitles = async () => {
+                const res = await fetch("/api/chatThreads", {
+                    method: "GET"
+                });
+                setTitle(await res.json());
+            };
+        },
+        [title]);
+
+
   return (
     <div className={"flex flex-col  relative max-h-[90%] px-1"}>
       <div className={"flex flex-row items-center justify-between p-2"}>
@@ -28,7 +37,7 @@ export function ChatThreads() {
         </Button>
       </div>
       <ScrollArea className="h-full max-h-[80%] rounded-md p-2">
-        {tags.map((tag) => (
+        {title.map((title: string) => (
           <React.Fragment key={tag}>
             <div
               onClick={() => alert("ciao")}
@@ -51,4 +60,68 @@ export function ChatThreads() {
       </ScrollArea>
     </div>
   );
+}*/
+export function ChatThreads() {
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    const [titles, setTitles] = useState([]);
+
+    useEffect(() => {
+
+        const fetchTitles = async () => {
+
+            const res = await fetch("/api/chatThreads", {
+                method: "GET"
+
+            });
+
+            const data = await res.json(); // fa crashare tutto
+            console.log("risposta api: ",data);
+            
+            setTitles(data);
+            if (data.length > 0) {
+                console.log('We have some results');
+            }
+            else {
+                console.log('pazzesco non abbiamo risultati ');
+            }
+        };
+        fetchTitles().then(() => setIsLoading(false));
+    }, []);
+
+
+
+    return (
+        <div className={"flex flex-col  relative max-h-[90%] px-1"}>
+            <div className={"flex flex-row items-center justify-between p-2"}>
+                <Label className={"text-lg"}>Nuova Chat</Label>
+                <Button variant={"ghost"} size={"icon"}>
+                    <Pencil2Icon className={"w-6 h-6"} />
+                </Button>
+            </div>
+            <ScrollArea className="h-full max-h-[80%] rounded-md p-2">
+                {titles.map((title : string) => (
+                    <React.Fragment key={title}>
+                        <div
+                            onClick={() => alert("ciao")}
+                            className="flex flex-row items-center justify-between text-sm transition-colors  hover:bg-background/50 rounded-md p-2 my-1"
+                        >
+                            {title}
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant={"ghost"} size={"icon"} className={"w-7 h-7"}>
+                                        <DotsHorizontalIcon />
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuItem>Elimina chat</DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
+                    </React.Fragment>
+                ))}
+            </ScrollArea>
+        </div>
+    );
 }
