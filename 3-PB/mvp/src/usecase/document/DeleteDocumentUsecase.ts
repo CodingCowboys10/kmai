c58711be7785b7f2c874f0fdd4af68c533208659
Andrea Cecchin <146743188+andreacecchin@users.noreply.github.com>
2024-03-19 19:27:00 +0100

    Update DeleteDocumentUsecase.ts

--- 3-PB/mvp/src/usecase/document/DeleteDocumentUsecase.ts
+++ 3-PB/mvp/src/usecase/document/DeleteDocumentUsecase.ts
@@ -1,17 +1,22 @@
-import type {IDocumentRepository, IUsecase} from "@/lib/config/interfaces";
+import type {IDocumentRepository, IUsecase, IEmbeddingRepository} from "@/lib/config/interfaces";
 import {injectable, inject} from "tsyringe";
 
 @injectable()
 class DeleteDocumentUsecase implements IUsecase<{docName: string, model: string }, void>{
     private readonly _documentRepository : IDocumentRepository;
+    private readonly _embeddingRepository: IEmbeddingRepository;
 
-    constructor(@inject("documentRepository")documentRepository: IDocumentRepository) {
+    constructor(@inject("documentRepository")documentRepository: IDocumentRepository,
+                @inject("embeddingRepository") embeddingRepository: IEmbeddingRepository,) {
         this._documentRepository = documentRepository;
+        this._embeddingRepository = embeddingRepository;
     }
 
     async execute({docName, model}: {docName: string, model: string }) {
-        return this._documentRepository.deleteDocument(docName, model);
+        await this._documentRepository.deleteDocument(docName, model);
+        const ids = await this._embeddingRepository.getIdsEmbedding(docName, model);
+        await this._embeddingRepository.deleteEmbedding(ids, model);
     }
 }
 
-export {DeleteDocumentUsecase}
+export {DeleteDocumentUsecase};
